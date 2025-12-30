2207598fbe624f7a1c92ac8c9b642295d58b84c7
Vaishnavi <vaishnavi2073@gmail.com>
2025-12-30 15:11:40 +0530

    Change background color of Journey section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -52,7 +52,7 @@ const About = () => {
 
 
       {/* Journey Section */}
-      <section className="py-16 bg-background">
+      <section className="py-16 bg-muted-background">
         <div className="container mx-auto px-4 lg:px-8">
           <div className="grid lg:grid-cols-2 gap-12 items-center">
             <div className="relative">
